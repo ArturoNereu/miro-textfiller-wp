94f27b332801863c18bb85abcb03e697595fb642
Arturo Nereu <hdgam3r@gmail.com>
2022-01-11 19:23:31 -0600

    Added base files

---
+++ main.js
@@ -0,0 +1,22 @@
+const icon = '<path fill-rule="evenodd" d="M0 .5A.5.5 0 0 1 .5 0h4a.5.5 0 0 1 0 1h-4A.5.5 0 0 1 0 .5Zm0 2A.5.5 0 0 1 .5 2h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5Zm9 0a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5Zm-9 2A.5.5 0 0 1 .5 4h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5Zm5 0a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5Zm7 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5Zm-12 2A.5.5 0 0 1 .5 6h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5Zm8 0a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5Zm-8 2A.5.5 0 0 1 .5 8h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5Zm7 0a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5Zm-7 2a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5Zm0 2a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5Zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5Z"/>'
+const dummyText = "Art party wolf consectetur reprehenderit cillum intelligentsia. Vinyl sriracha nostrud pickled neutra migas umami master cleanse small batch occaecat ipsum. Cornhole semiotics unicorn selfies selvage gluten-free single-origin coffee organic cardigan poutine. Aute viral paleo disrupt dreamcatcher."
+
+miro.onReady(() => {
+  miro.initialize({
+    extensionPoints: {
+      bottomBar: {
+        title: 'Text Filler',
+        svgIcon: icon,
+        positionPriority: 1,
+        onClick: async () => {
+
+        let shape = (await miro.board.widgets.get({type:'shape'}))[0]
+        await miro.board.viewport.zoomToObject(shape)
+        miro.board.widgets.update({id: shape.id, text: dummyText, style:{shapeBackgroundColor:'#7ac673'}})
+
+        miro.showNotification('Dummy Text Added to your Shape!')
+        },
+      },
+    },
+  })
+})
